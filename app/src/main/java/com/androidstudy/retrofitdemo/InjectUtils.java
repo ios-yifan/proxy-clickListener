8709a59dba5b5165c83a5ab11ad03e01842e30a7
zhangyifan <zhangyifan@rongcloud.cn>
2021-01-22 18:28:04 +0800

    添加注释

--- app/src/main/java/com/androidstudy/retrofitdemo/InjectUtils.java
+++ app/src/main/java/com/androidstudy/retrofitdemo/InjectUtils.java
@@ -28,18 +28,30 @@ public class InjectUtils {
                 //判断当前注解是否有 eventType 的元注解
                 if (annotationType.isAnnotationPresent(EventType.class)){
 
+                    // 获取 当前注解中 EventType注解的注解信息  annotationType拿到的是 OnClick 注解，通过 onClick 注解再去拿 EventType 注解
                     EventType eventType = annotationType.getAnnotation(EventType.class);
 
+                    // 获取到具体的类信息
                     Class listenerType = eventType.listenerType();
                     String listenerSetter = eventType.listenerSetter();
 
                     try {
+                        // 两种方式
+                        // 1. 获取OnClick 的 value 值
                         Method valueMethod = annotationType.getDeclaredMethod("value");
                         int[] viewIds = (int[]) valueMethod.invoke(annotation);
 
+                        // 2
+                        //int[] ids = ((OnClick) annotation).value();
+
+
+                        // 构建一个接口的实现类 方便保存 target（就是 activity），保证按钮的点击事件响应到正确的 activity
                         ListenerInvocationHandler<Activity> listenerInvocationHandler = new ListenerInvocationHandler(valueMethod,mainActivity);
+
+                        // 生成一个代理对象，
                         Object proxyInstance = Proxy.newProxyInstance(listenerType.getClassLoader(), new Class[]{listenerType}, listenerInvocationHandler);
 
+                        // 遍历 ID 获取到 view，给 view 添加
                         for (int viewId : viewIds) {
                             View viewById = mainActivity.findViewById(viewId);
                             Method setter = viewById.getClass().getMethod(listenerSetter,listenerType);
